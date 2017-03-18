21beaebf07dbe9b451abddc431d1a8fe43b8ee50
aflores92 <aflores@asdsoftlabs.com>
2017-03-17 23:16:10 -0400

    2.1

---
+++ app/src/main/java/com/andresflores/taxicementerio/Main4Activity.java
@@ -0,0 +1,79 @@
+package com.andresflores.taxicementerio;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class Main4Activity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText textEmail;
+    private EditText textPass;
+    private Button btnRegister;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+
+        progressDialog = new ProgressDialog(this);
+        textEmail = (EditText) findViewById(R.id.etxt_email);
+        textPass = (EditText) findViewById(R.id.etxt_password);
+        btnRegister = (Button) findViewById(R.id.btn_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
+            }
+        });
+
+
+    }
+
+
+
+    private void doLogin() {
+        String email = textEmail.getText().toString().trim();
+        String password = textPass.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            progressDialog.setMessage("Loging , please wait");
+            progressDialog.show();
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Main4Activity.this, "Login succesful", Toast.LENGTH_SHORT).show();
+
+
+                            } else
+                                Toast.makeText(Main4Activity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+
+}
