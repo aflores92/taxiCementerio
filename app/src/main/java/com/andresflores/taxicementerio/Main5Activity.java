375f98615414f123c8dab008c1f05814d2decd7f
aflores92 <aflores@asdsoftlabs.com>
2017-03-28 18:27:12 -0400

    Cambios lunes incluyen mapa de taxista

--- app/src/main/java/com/andresflores/taxicementerio/Main5Activity.java
+++ app/src/main/java/com/andresflores/taxicementerio/Main5Activity.java
@@ -3,6 +3,7 @@ package com.andresflores.taxicementerio;
 import android.app.Activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -10,6 +11,8 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,6 +20,7 @@ import android.widget.ListView;
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
 import com.firebase.geofire.LocationCallback;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -38,8 +42,10 @@ public class Main5Activity extends Activity implements LocationListener {
     ArrayAdapter arrayAdapter;
     private LocationManager locationManager;
     ArrayList<String> data = new ArrayList<>();
-
-
+    ArrayList<String> usuario;
+    ArrayList<Double> latitud;
+    ArrayList<Double> longitud;
+    Location location;
 
 
 
@@ -48,8 +54,12 @@ public class Main5Activity extends Activity implements LocationListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        usuario = new ArrayList<String>();
+        latitud = new ArrayList<Double>();
+        longitud = new ArrayList<Double>();
 
         listView = (ListView) findViewById(R.id.listView);
+
         listViewContent = new ArrayList<String>();
         listViewContent.add("Prueba");
         arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listViewContent);
@@ -74,27 +84,83 @@ public class Main5Activity extends Activity implements LocationListener {
             return;
         }
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1, this);
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
         //  if (location != null) {
 
-        updateLocation(location);
+        updateLocation();
         // }0
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(getApplicationContext(),MapsActivity2.class);
+                i.putExtra("usuarioLatitud",location.getLatitude());
+                i.putExtra("usuarioLongitud",location.getLongitude());
+                startActivity(i);
+
+
+            }
+        });
 
 
 
     }
 
-    public void updateLocation(Location location) {
+    public void updateLocation() {
+
+
 
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Pedidos");
+        ref.child("geofire");
 
-        ref.addChildEventListener(new ChildEventListener() {
+        GeoFire geoFire = new GeoFire(ref.child("Localizacion"));
+
+
+        //GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(location.getLatitude(),location.getLongitude()), 0.6);
+/*
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                listViewContent.clear();
+                listViewContent.add(key);
+
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+
+            }
+        });
+
+        for (String objet:listViewContent){
+
+            Log.v("prueba_2",objet);
+
+        }*/
+
+
+        /*ref.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
                 Map<String,String> map = dataSnapshot.getValue(Map.class);
-
 
                 String values = dataSnapshot.getValue(String.class);
                 data.add(values);
@@ -120,19 +186,15 @@ public class Main5Activity extends Activity implements LocationListener {
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });
+        });*/
 
-        for (String datos : data){
+        /*for (String datos : data){
 
             Log.v("Prueba",datos);
 
         }
 
-
-        GeoFire geoFire = new GeoFire(ref);
-
-
-        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(37.7832, -122.4056), 0.6);
+*/
 
 
       //  DatabaseReference mUserGeo = mPedidoU.orderByChild();
@@ -140,7 +202,7 @@ public class Main5Activity extends Activity implements LocationListener {
        // GeoFire geoFire = new GeoFire(mUserGeo);
 
        // geoFire.setLocation("Localizacion", new GeoLocation(location.getLatitude(),location.getLongitude()));
-        geoFire.getLocation("Localizacion", new LocationCallback() {
+        /*geoFire.getLocation("Localizacion", new LocationCallback() {
             @Override
             public void onLocationResult(String key, GeoLocation location) {
 
@@ -150,7 +212,7 @@ public class Main5Activity extends Activity implements LocationListener {
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });
+        });*/
 
 
 
@@ -159,7 +221,7 @@ public class Main5Activity extends Activity implements LocationListener {
     @Override
     public void onLocationChanged(Location location) {
 
-        updateLocation(location);
+        updateLocation();
 
 
     }
